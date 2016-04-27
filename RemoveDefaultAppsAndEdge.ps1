fd2299b289cd5b39bff7d482a428d2c39e266193
Benjamin Farley <benalfarley@gmail.com>
2016-04-27 11:50:33 +0100

    Update RemoveDefaultAppsAndEdge.ps1
    Added Synopsis and Description

--- RemoveDefaultAppsAndEdge.ps1
+++ RemoveDefaultAppsAndEdge.ps1
@@ -1,8 +1,26 @@
-## NOTE: This script will not remove the tiles from the start menu on the account
-## that this script is run.
-## You will also still have some of the pending apps that need to be un-pinned on new user accounts
+<#
+.SYNOPSIS
+    Removes builtin apps and Microsoft Edge from Windows 10
+    
+.DESCRIPTION
+    Queries and removes specified Builtin Apps if they are installed, then deletes Edge from Windows 10
+    
+    This Script requires administrator privileges.
+    
+    You can run this script from Command prompt using the following:
 
-## Add any applications "DisplayName" that you would like to remove into this list.
+        powershell.exe -ExecutionPolicy Unrestricted -NoLogo -NoProfile -Command "& '.\RemoveDefaultApps.ps1'"
+
+    This script will not remove the tiles from the start menu on the account that this script is run.
+    You will also still have some of the pending apps that need to be un-pinned on new user accounts.
+
+    This script might need additional editing if you require additional apps to be removed. Run the following
+    command for a list of installed apps and add the app name into the $AppsForRemoval list:
+
+        Get-AppXPackage | Select Name | Sort Name
+#>
+
+
 $AppsForRemoval='Microsoft.3DBuilder', 'Microsoft.BingFinance', 'Microsoft.BingNews', 'Microsoft.BingSports', 
 'Microsoft.BingWeather', 'Microsoft.ConnectivityStore', 'Microsoft.Getstarted', 'Microsoft.Messaging', 
 'Microsoft.MicrosoftOfficeHub', 'Microsoft.MicrosoftSolitaireCollection', 'Microsoft.Office.OneNote', 
@@ -11,18 +29,13 @@ $AppsForRemoval='Microsoft.3DBuilder', 'Microsoft.BingFinance', 'Microsoft.BingN
 'Microsoft.WindowsMaps', 'Microsoft.WindowsSoundRecorder', 'Microsoft.WindowsStore', 'Microsoft.XboxApp', 
 'Microsoft.XboxGameCallableUI', 'Microsoft.XboxIdentityProvider', 'Microsoft.ZuneMusic', 'Microsoft.ZuneVideo'
 
-#Remove from Image
 Get-AppxProvisionedPackage -Online | Where-Object DisplayName -In $AppsForRemoval |
 Remove-ProvisionedAppxPackage -Online | Out-Null
 
-#Remove from Users
 Get-AppxPackage -AllUsers | Where-Object Name -In $AppsForRemoval |
 Remove-AppxPackage | Out-Null
 
-
-## Comment this secton of code if you do not want to remove Edge.
-## You can "re-install" edge by adding back in the two .exe files
-## from a standard build of Windows 10
+# Remove Edge
 
 $Directory = "C:\Windows\SystemApps\Microsoft.MicrosoftEdge_8wekyb3d8bbwe"
 $Acl = Get-Acl $Directory
